a464818af1f42bdb164c0e04373162fc794591ed
Roy Mcqueen <roy.mcqueen@meijer.com>
2019-11-14 11:51:10 -0500

    keychain helper test

--- app/src/main/java/com/example/nextsoundz/SettingsDialogActivity.kt
+++ app/src/main/java/com/example/nextsoundz/SettingsDialogActivity.kt
@@ -8,7 +8,8 @@ class SettingsDialogActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//Just checking to see if this is working
+//Just checking to see if this is working1
+        //checking again to see if the keychane helper is woarking
     }
 
 
